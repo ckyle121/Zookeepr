f416c9bd0653730ce97d1bd5491c9062210c9798
ckyle121 <carolinekyle21@gmail.com>
2022-02-20 09:15:03 -0600

    add environmental variable for PORT

--- server.js
+++ server.js
@@ -2,11 +2,10 @@
 const express = require('express');
 const { animals } = require('./data/animals.json');
 
+const PORT = process.env.PORT || 3001
 const app = express();
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
-});
+
 
 function filterByQuery(query, animalsArray){
     let personalityTraitsArray = [];
@@ -46,3 +45,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}`);
+});
