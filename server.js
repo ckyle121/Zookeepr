e5dbf327a4a8033937bc39ef3648c1639249c297
ckyle121 <carolinekyle21@gmail.com>
2022-02-20 09:01:10 -0600

    Add Heroku

---
+++ server.js
@@ -0,0 +1,48 @@
+// import express package 
+const express = require('express');
+const { animals } = require('./data/animals.json');
+
+const app = express();
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001`);
+});
+
+function filterByQuery(query, animalsArray){
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+    if (query.personalityTraits) {
+        // save personality traits as dedicated array
+        if (typeof query.personalityTraits === 'string') {
+          personalityTraitsArray = [query.personalityTraits];
+        } else {
+          personalityTraitsArray = query.personalityTraits;
+        }
+        // loop through each trait in the personality traits array
+        personalityTraitsArray.forEach(trait => {
+          filteredResults = filteredResults.filter(
+            animal => animal.personalityTraits.indexOf(trait) !== -1
+          );
+        });
+      }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    // return filtered results
+    return filteredResults;
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    if (req.query){
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
